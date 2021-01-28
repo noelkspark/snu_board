55dab042de5f77536f2de99651e31114dd79a72f
noelkspark <51874193+noelkspark@users.noreply.github.com>
2021-01-29 00:51:41 +0900

    got list of urls from board elem
    getting list of urls from board elem
    completed

--- ts_web_Scraping/src/index.ts
+++ ts_web_Scraping/src/index.ts
@@ -2,19 +2,27 @@ import axios from "axios";
 import cheerio from "cheerio";
 
 const main_url = "http://ie.snu.ac.kr/";
-const main_notice = "http://ie.snu.ac.kr/ko/board/2";
+const notice_main = "http://ie.snu.ac.kr/ko/board/2";
 const AxiosInst = axios.create();
 
-function get_url(URL) {
+function get_element_urls(URL) {
+  let res = [];
   AxiosInst.get(URL)
     .then((response) => {
-      const html = response.data; // Get the HTML from the HTTP request
-      const $ = cheerio.load(html); // Load the HTML string into cheerio
-      const $title_carriage = $("tbody > tr");
-      const $title = $title_carriage.find($(".views-field-title-field"));
-      console.log($title.text()); // Log the number of captured elements
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const $board_element = $("tbody > tr > td > a");
+      //const $board_element_url = $board_element.attr("href");
+      $board_element.each((index, value) => {
+        const link = $(value).attr("href");
+        res.push(link);
+      });
+      //console.log($board_element_url);
+      for (let i = 0; i < res.length; i++) {
+        console.log(res[i]);
+      }
     })
     .catch(console.error);
 }
 
-get_url(main_notice);
+get_element_urls(notice_main);
