a5be5bca7ca1bfb61e498f62e6faa60bccda2148
noelkspark <51874193+noelkspark@users.noreply.github.com>
2021-01-31 21:28:25 +0900

    done with ie-main notice

--- ts_web_Scraping/src/index.ts
+++ ts_web_Scraping/src/index.ts
@@ -1,13 +1,16 @@
 import axios from "axios";
 import cheerio from "cheerio";
 
-const main_url: string = "http://ie.snu.ac.kr";
-const notice_main = "http://ie.snu.ac.kr/ko/board/2";
+const ie_main_url: string = "http://ie.snu.ac.kr";
+const ie_notice_main: string = "http://ie.snu.ac.kr/ko/board/2";
 
-let main_notice_urlList = [];
-let main_notice_urlTitle = [];
+let ie_main_notice_urlList = [];
+let ie_main_notice_urlTitle = [];
+let ie_main_notice_urlDate = [];
+let ie_main_notice_urlContent = [];
 
 async function get_element_urls(url: string) {
+  //////////get urls from some boards
   const AxiosInst = axios.create();
   await AxiosInst.get(url)
     .then((response) => {
@@ -17,37 +20,41 @@ async function get_element_urls(url: string) {
 
       $board_element.each((index, value) => {
         const $link = $(value).attr("href");
-        main_notice_urlList.push($link);
+        ie_main_notice_urlList.push($link);
       });
     })
     .catch(console.error);
 }
 
 async function crawl_info() {
-  await get_element_urls(notice_main);
-  const len = main_notice_urlList.length;
+  /////////////////////get info from crawled urls from <function : get_element_urls>
+  await get_element_urls(ie_notice_main); ////////title, date, and content
+  const len = ie_main_notice_urlList.length;
 
   for (let i = 0; i < len; i++) {
     const AxiosInst = axios.create();
-    await AxiosInst.get(main_url + main_notice_urlList[i])
+    await AxiosInst.get(ie_main_url + ie_main_notice_urlList[i])
       .then(async (response) => {
         const html = response.data;
         const $ = cheerio.load(html);
         const $title = $("div.field-item > h2");
-
-        main_notice_urlTitle.push($title.text());
-        console.log(main_notice_urlTitle);
+        const $date = $("div.field-name-post-date > .field-items");
+        const $content = $("div.field-items > .field-item");
+        ie_main_notice_urlTitle.push($title.text());
+        ie_main_notice_urlDate.push($date.text());
+        ie_main_notice_urlContent.push($content.text());
+        console.log(ie_main_notice_urlContent);
       })
       .catch();
   }
 }
 
 async function debug_print() {
-  await get_element_urls(notice_main);
+  await get_element_urls(ie_notice_main);
 
-  console.log(main_notice_urlList.length);
-  for (let i = 0; i < main_notice_urlList.length; i++) {
-    console.log(main_notice_urlList[i]);
+  console.log(ie_main_notice_urlList.length);
+  for (let i = 0; i < ie_main_notice_urlList.length; i++) {
+    console.log(ie_main_notice_urlList[i]);
   }
 }
 
