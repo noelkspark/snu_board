1dc5fb5b57948d677a942a62334266d21fb14fb7
noelkspark <51874193+noelkspark@users.noreply.github.com>
2021-01-31 21:11:24 +0900

    got main notice titles

--- ts_web_Scraping/src/index.ts
+++ ts_web_Scraping/src/index.ts
@@ -1,28 +1,54 @@
 import axios from "axios";
 import cheerio from "cheerio";
 
-const main_url = "http://ie.snu.ac.kr/";
+const main_url: string = "http://ie.snu.ac.kr";
 const notice_main = "http://ie.snu.ac.kr/ko/board/2";
-const AxiosInst = axios.create();
 
-function get_element_urls(URL) {
-  let res = [];
-  AxiosInst.get(URL)
+let main_notice_urlList = [];
+let main_notice_urlTitle = [];
+
+async function get_element_urls(url: string) {
+  const AxiosInst = axios.create();
+  await AxiosInst.get(url)
     .then((response) => {
       const html = response.data;
       const $ = cheerio.load(html);
       const $board_element = $("tbody > tr > td > a");
-      //const $board_element_url = $board_element.attr("href");
+
       $board_element.each((index, value) => {
-        const link = $(value).attr("href");
-        res.push(link);
+        const $link = $(value).attr("href");
+        main_notice_urlList.push($link);
       });
-      //console.log($board_element_url);
-      for (let i = 0; i < res.length; i++) {
-        console.log(res[i]);
-      }
     })
     .catch(console.error);
 }
 
-get_element_urls(notice_main);
+async function crawl_info() {
+  await get_element_urls(notice_main);
+  const len = main_notice_urlList.length;
+
+  for (let i = 0; i < len; i++) {
+    const AxiosInst = axios.create();
+    await AxiosInst.get(main_url + main_notice_urlList[i])
+      .then(async (response) => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const $title = $("div.field-item > h2");
+
+        main_notice_urlTitle.push($title.text());
+        console.log(main_notice_urlTitle);
+      })
+      .catch();
+  }
+}
+
+async function debug_print() {
+  await get_element_urls(notice_main);
+
+  console.log(main_notice_urlList.length);
+  for (let i = 0; i < main_notice_urlList.length; i++) {
+    console.log(main_notice_urlList[i]);
+  }
+}
+
+crawl_info();
