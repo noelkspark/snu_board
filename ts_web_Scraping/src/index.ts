4607012547cf8c2aef2cf2deeb49262a006aceb7
noelkspark <51874193+noelkspark@users.noreply.github.com>
2021-01-31 22:57:08 +0900

    got all needed urls
    need to :
    change the get_element_url function so that it works for any urls

--- ts_web_Scraping/src/index.ts
+++ ts_web_Scraping/src/index.ts
@@ -2,7 +2,16 @@ import axios from "axios";
 import cheerio from "cheerio";
 
 const ie_main_url: string = "http://ie.snu.ac.kr";
-const ie_notice_main: string = "http://ie.snu.ac.kr/ko/board/2";
+const ie_main_news: string = "http://ie.snu.ac.kr/ko/board/2";
+const ie_notice_UD: string = "http://ie.snu.ac.kr/ko/board/7";
+const ie_notice_GS: string = "http://ie.snu.ac.kr/ko/board/8";
+const ie_notice_recruit: string = "http://ie.snu.ac.kr/ko/board/6";
+const ie_notice_scholship: string = "http://ie.snu.ac.kr/ko/board/15";
+
+const eng_main_url: string = "https://eng.snu.ac.kr";
+const eng_notice: string = "https://eng.snu.ac.kr/notice";
+const eng_scholship: string = "https://eng.snu.ac.kr/janghak";
+const eng_recruit: string = "https://eng.snu.ac.kr/recruit";
 
 let ie_main_notice_urlList = [];
 let ie_main_notice_urlTitle = [];
@@ -26,9 +35,9 @@ async function get_element_urls(url: string) {
     .catch(console.error);
 }
 
-async function crawl_info() {
+async function ie_crawl_info(board_url: string) {
   /////////////////////get info from crawled urls from <function : get_element_urls>
-  await get_element_urls(ie_notice_main); ////////title, date, and content
+  await get_element_urls(board_url); ////////title, date, and content
   const len = ie_main_notice_urlList.length;
 
   for (let i = 0; i < len; i++) {
@@ -37,20 +46,20 @@ async function crawl_info() {
       .then(async (response) => {
         const html = response.data;
         const $ = cheerio.load(html);
-        const $title = $("div.field-item > h2");
-        const $date = $("div.field-name-post-date > .field-items");
-        const $content = $("div.field-items > .field-item");
-        ie_main_notice_urlTitle.push($title.text());
-        ie_main_notice_urlDate.push($date.text());
-        ie_main_notice_urlContent.push($content.text());
-        console.log(ie_main_notice_urlContent);
+        const $title = $("div.field-item > h2"); //get title
+        const $date = $("div.field-name-post-date > .field-items"); //get date
+        const $content = $("div.field-items > .field-item"); // get full content
+        ie_main_notice_urlTitle.push($title.text()); //save in <list : ie_main_notice_urlTitle>
+        ie_main_notice_urlDate.push($date.text()); //save in <list : ie_main_notice_urlDate>
+        ie_main_notice_urlContent.push($content.text()); //save in <list : ie_main_notice_urlContent>
+        //console.log(ie_main_notice_urlTitle);
       })
       .catch();
   }
 }
 
 async function debug_print() {
-  await get_element_urls(ie_notice_main);
+  await get_element_urls(ie_main_news);
 
   console.log(ie_main_notice_urlList.length);
   for (let i = 0; i < ie_main_notice_urlList.length; i++) {
@@ -58,4 +67,4 @@ async function debug_print() {
   }
 }
 
-crawl_info();
+ie_crawl_info(ie_main_news);
